a3e6a4e54206484fa386d63214bb02e5a817c222
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2025-05-22 14:45:10 +0300

    feat: add LD50 dataset + training loop

--- notebooks/AttentiveFP/__init__.py
+++ notebooks/AttentiveFP/__init__.py
@@ -1,3 +1,3 @@
 from .AttentiveLayers import Fingerprint
 from .AttentiveLayers_viz import Fingerprint_viz
-from .getFeatures import save_smiles_dicts, get_smiles_dicts, get_smiles_array, moltosvg_highlight
+from .getFeatures import save_smiles_dicts, get_smiles_dicts, get_smiles_array, moltosvg_highlight, num_atom_features, num_bond_features
