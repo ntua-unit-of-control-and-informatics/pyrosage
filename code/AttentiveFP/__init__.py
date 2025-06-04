6ac9caa7bcc612158bec5c259750ff1dec448b9e
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2025-06-04 10:16:56 +0300

    chore: remove obsolete files

--- code/AttentiveFP/__init__.py
+++
@@ -1,3 +0,0 @@
-from .AttentiveLayers import Fingerprint
-from .AttentiveLayers_viz import Fingerprint_viz
-from .getFeatures import save_smiles_dicts, get_smiles_dicts, get_smiles_array, moltosvg_highlight, num_atom_features, num_bond_features
